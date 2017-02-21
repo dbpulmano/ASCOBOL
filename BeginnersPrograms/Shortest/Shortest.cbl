961271f5c48e55b5f8d83d98a11f587a1e9bbbdd
Dana Pulmano <dbpulmano@student.apc.edu.ph>
2017-02-21 10:08:14 +0800

    Added Beginner's Programsfolder

---
+++ BeginnersPrograms/Shortest/Shortest.cbl
@@ -0,0 +1,15 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ShortestProgram.
+
+       PROCEDURE DIVISION.
+       DisplayPrompt.
+           DISPLAY "I did it".
+           DISPLAY "HELLO COBOL"
+           STOP RUN.
+       END PROGRAM ShortestProgram.
