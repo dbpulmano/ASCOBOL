b820d11498a785a849123fe1261f16ec8f68f2fa
Dana Pulmano <dbpulmano@student.apc.edu.ph>
2017-02-21 11:14:22 +0800

    Modified SeqRpt

--- SequentialFiles/SeqRpt/SeqRpt.cbl
+++ SequentialFiles/SeqRpt/SeqRpt.cbl
@@ -5,7 +5,7 @@
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. SeqRpt.
+       PROGRAM-ID. StudentNumbersReport.
 
 
        ENVIRONMENT DIVISION.
@@ -26,31 +26,34 @@
            88 EndOfStudentFile VALUE HIGH-VALUES.
            02 StudentId        PIC 9(7).
            02 StudentName.
-           03 Surname       PIC X(8).
-           03 Initials      PIC XX.
+               03 Surname      PIC X(8).
+               03 Initials     PIC XX.
            02 DateOfBirth.
-           03 YOBirth       PIC 9(4).
-           03 MOBirth       PIC 9(2).
-           03 DOBirth       PIC 9(2).
-           02  CourseCode        PIC X(4).
-           02  Gender            PIC X.
-           88 Male           VALUE "M", "m".
-
+               03 YOBirth      PIC 9(4).
+               03 MOBirth      PIC 9(2).
+               03 DOBirth      PIC 9(2).
+           02  CourseCode      PIC X(4).
+           02  Gender          PIC X.
+               88 Male         VALUE "M", "m".
 
        FD  ReportFile.
-           01  PrintLine            PIC X(40).
+           01  PrintLine       PIC X(40).
 
        WORKING-STORAGE SECTION.
        01  HeadingLine          PIC X(21) VALUE "  Record Count Report".
+
        01  StudentTotalLine.
            02  FILLER           PIC X(17) VALUE "Total Students = ".
            02  PrnStudentCount  PIC Z,ZZ9.
+
        01  MaleTotalLine.
            02  FILLER           PIC X(17) VALUE "Total Males    = ".
            02  PrnMaleCount     PIC Z,ZZ9.
+
        01  FemaleTotalLine.
            02  FILLER           PIC X(17) VALUE "Total Females  = ".
            02  PrnFemaleCount   PIC Z,ZZ9.
+
        01  WorkTotals.
            02  StudentCount     PIC 9(4) VALUE ZERO.
            02  MaleCount        PIC 9(4) VALUE ZERO.
@@ -62,23 +65,24 @@
        Begin.
            OPEN INPUT StudentFile
            OPEN OUTPUT ReportFile
+
            READ StudentFile
                AT END SET EndOfStudentFile TO TRUE
            END-READ
            PERFORM UNTIL EndOfStudentFile
                ADD 1 TO StudentCount
                IF Male  ADD 1 TO MaleCount
-               ELSE   ADD 1 TO FemaleCount
+                   ELSE   ADD 1 TO FemaleCount
                END-IF
                READ StudentFile
-               AT END SET EndOfStudentFile TO TRUE
+                   AT END SET EndOfStudentFile TO TRUE
                END-READ
            END-PERFORM
 
-       PERFORM PrintReportLines
+           PERFORM PrintReportLines
 
-       CLOSE StudentFile, ReportFile
-       STOP RUN.
+           CLOSE StudentFile, ReportFile
+           STOP RUN.
 
        PrintReportLines.
            MOVE StudentCount TO PrnStudentCount
@@ -97,6 +101,6 @@
            WRITE PrintLine FROM FemaleTotalLine
                    AFTER ADVANCING 2 LINES.
 
-           STOP RUN.
 
-       END PROGRAM SeqRpt.
+
+       END PROGRAM StudentNumbersReport.
